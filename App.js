f94dcb39bad7201e202933f4e5c328d8066515d8
FurkanAndac <Furkan_andac_@hotmail.com>
2018-11-11 17:10:07 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { StyleSheet, Platform, Image, Text, View } from 'react-native'
+import { SwitchNavigator } from 'react-navigation'
+// import the different screens
+import Loading from "./screens/Loading";
+import SignUp from "./screens/SignUp";
+import Login from "./screens/Login";
+import Main from "./screens/Main";
+
+// create our app's navigation stack
+const App = SwitchNavigator(
+  {
+    Loading,
+    SignUp,
+    Login,
+    Main
+  },
+  {
+    initialRouteName: 'Loading'
+  }
+)
+export default App
---
+++ screens/Loading.js
@@ -0,0 +1,29 @@
+// Loading.js
+import React from 'react'
+import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
+import {auth} from "react-native-firebase";
+
+
+export default class Loading extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Loading</Text>
+        <ActivityIndicator size="large" />
+      </View>
+    )
+  }
+
+  componentDidMount() {
+    auth().onAuthStateChanged(user => {
+      this.props.navigation.navigate(user ? 'Main' : 'SignUp')
+    })
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
